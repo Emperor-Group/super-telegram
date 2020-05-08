ab98cba444e1b990423685fdaecf74b5f5d08689
aa_rodriguezv <aa.rodriguezv@uniandes.edu.co>
2020-05-07 21:59:11 -0500

    Future Builder implemented

--- shop_app/lib/screens/orders_screen.dart
+++ shop_app/lib/screens/orders_screen.dart
@@ -4,66 +4,60 @@ import 'package:shop_app/providers/orders.dart' show Orders;
 import 'package:shop_app/widgets/main_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
+
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isInit = false;
-  bool _isLoading = false;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (!_isInit) {
-      setState(() {
-        _isLoading = true;
-      });
-      Provider.of<Orders>(context).fetchAndSetOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-    _isInit = true;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context);
-    var theOrders = orders.ordersList;
-
     return Scaffold(
       drawer: MainDrawer(),
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : theOrders.length <= 0
-              ? RefreshIndicator(
-                  child: Center(
-                    child: Text(
-                      'No Orders Here Yet!\nStart Placing Some!',
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                  ),
-                  onRefresh: () => Provider.of<Orders>(context, listen: false)
-                      .fetchAndSetOrders(),
-                )
-              : RefreshIndicator(
-                  child: ListView.builder(
-                    itemBuilder: (ctx, index) => OrderItem(theOrders[index]),
-                    itemCount: theOrders.length,
-                  ),
-                  onRefresh: () => Provider.of<Orders>(context, listen: false)
-                      .fetchAndSetOrders(),
-                ),
+            );
+          } else {
+            if (snapshot.error == null) {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) {
+                  return orderData.ordersList.length <= 0
+                      ? RefreshIndicator(
+                          child: Center(
+                            child: Text(
+                              'No Orders Here Yet!\nStart Placing Some!',
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                          ),
+                          onRefresh: () =>
+                              Provider.of<Orders>(context, listen: false)
+                                  .fetchAndSetOrders(),
+                        )
+                      : RefreshIndicator(
+                          child: ListView.builder(
+                            itemBuilder: (ctx, index) =>
+                                OrderItem(orderData.ordersList[index]),
+                            itemCount: orderData.ordersList.length,
+                          ),
+                          onRefresh: () =>
+                              Provider.of<Orders>(context, listen: false)
+                                  .fetchAndSetOrders(),
+                        );
+                },
+              );
+            } else {
+              return Center(
+                child: Text('An error ocurred!'),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
